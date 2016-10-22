9c3fc9d1b5829eb0d6a46b0b31c560914afe0fa1
anhtu95 <anhtu23795@gmail.com>
2016-10-22 09:16:42 +0700

    [anhtu]

--- parser.c
+++ parser.c
@@ -41,51 +41,171 @@ void compileProgram(void) {
 void compileBlock(void) {
   assert("Parsing a Block ....");
   // TODO of anhtu
+	if(lookAhead->tokenType == KW_CONST){
+			compileConstDecls();
+	}	
+ 	if(lookAhead->tokenType == KW_TYPE){
+			compileTypeDecls();
+	}
+	if(lookAhead->tokenType == KW_VAR){
+			compileVarDecls();
+	}	
+	if(lookAhead->tokenType == KW_PROCEDURE){ 
+			compileProcDecl();
+	}
+	if(lookAhead->tokenType == KW_FUNCTION){
+		compileFuncDecl();
+	}
+	if(lookAhead->tokenType == KW_BEGIN){
+			compileStatements();
+	}
   assert("Block parsed!");
 }
 
 void compileConstDecls(void) {
   // TODO of anhtu
+	eat(KW_CONST);
+	eat(TK_IDENT);
+	eat(SB_EQ);
+	compileConstant();
+	eat(SB_SEMICOLON);
+	while(lookAhead->tokenType == TK_IDENT){
+		eat(TK_IDENT);
+		eat(SB_EQ);
+		compileConstant();
+		eat(SB_SEMICOLON);
+	}	
 }
 
 void compileTypeDecls(void) {
   // TODO of anhtu
+	eat(KW_TYPE);
+	eat(TK_IDENT);
+	eat(SB_EQ);
+	compileType();
+	eat(SB_SEMICOLON);
+	while(lookAhead->tokenType == TK_IDENT){
+		eat(TK_IDENT);
+		eat(SB_EQ);
+		compileType();
+		eat(SB_SEMICOLON);
+	}	
 }
 
 void compileVarDecls(void) {
   // TODO of anhtu
+	eat(KW_VAR);
+	eat(TK_IDENT);
+	eat(SB_COLON);
+	compileType();
+	eat(SB_SEMICOLON);
+	while(lookAhead->tokenType == TK_IDENT){
+		eat(TK_IDENT);
+		eat(SB_COLON);
+		compileType();
+		eat(SB_SEMICOLON);
+	}	
 }
 
 void compileProcDecl(void) {
   assert("Parsing a procedure ....");
   // TODO of anhtu
+	while(lookAhead->tokenType == KW_PROCEDURE){
+		eat(KW_PROCEDURE);
+		eat(TK_IDENT);
+		if(lookAhead->tokenType != SB_SEMICOLON){
+			compileParams();	
+		}
+		eat(SB_SEMICOLON);
+		compileBlock();
+		eat(SB_SEMICOLON);
+	}
+
   assert("Procedure parsed ....");
 }
 
 void compileFuncDecl(void) {
   assert("Parsing a function ....");
   // TODO of anhtu
+	while(lookAhead->tokenType == KW_PROCEDURE || lookAhead->tokenType == KW_FUNCTION){
+		if(lookAhead->tokenType == KW_PROCEDURE){
+			compileProcDecl();
+		}else{
+			eat(KW_FUNCTION);
+			eat(TK_IDENT);
+			if(lookAhead->tokenType != SB_COLON){
+				compileParams();
+			}
+			eat(SB_COLON);
+			compileBasicType();
+			eat(SB_SEMICOLON);
+			compileBlock();
+			eat(SB_SEMICOLON);
+		}	
+	}
   assert("Function parsed ....");
 }
 
 void compileParams(void) {
   // TODO of anhtu
+	while(true){
+		eat(SB_LPAR);
+		if(lookAhead->tokenType == KW_VAR){
+			eat(KW_VAR);		
+		}
+		eat(TK_IDENT);
+		eat(SB_COLON);
+		compileBasicType();
+		if(lookAhead->tokenType == SB_RPAR){
+			eat(SB_RPAR);
+			break;	
+		}
+		eat(SB_SEMICOLON);
+	}	
 }
 
 void compileUnsignedConstant(void) {
   // TODO of anhtu
+	switch(lookAhead->tokenType){
+		case TK_NUMBER: eat(TK_NUMBER); break;
+		case TK_CHAR : eat(TK_CHAR); break;
+		case TK_IDENT : eat(TK_IDENT); break;
+	}
 }
 
 void compileConstant(void) {
   // TODO of anhtu
+	if(lookAhead->tokenType == SB_PLUS || lookAhead->tokenType == SB_MINUS){
+		if(lookAhead->tokenType == SB_PLUS){
+			eat(SB_PLUS);
+		}else if(lookAhead->tokenType == SB_MINUS){
+			eat(SB_MINUS);
+		}
+		if(lookAhead->tokenType == TK_IDENT){
+			eat(TK_IDENT);
+		}else if(lookAhead->tokenType == TK_NUMBER){
+			eat(TK_NUMBER);
+		}
+	}else if(lookAhead->tokenType == TK_CHAR){
+		eat(TK_CHAR);
+	}
 }
 
 void compileType(void) {
   // TODO of anhtu
+	switch(lookAhead->tokenType){
+		case TK_IDENT : eat(TK_IDENT); break;
+		case KW_ARRAY : eat(KW_ARRAY); eat(SB_LSEL); eat(TK_NUMBER); eat(SB_RSEL); eat(KW_OF); compileType(); break;
+		default : compileBasicType();
+	}
 }
 
 void compileBasicType(void) {
   // TODO of anhtu
+	switch(lookAhead->tokenType){
+		case KW_INTEGER : eat(KW_INTEGER); break;
+		case KW_CHAR : eat(KW_CHAR); break;	
+	}
 }
 
 void compileStatements(void) {
